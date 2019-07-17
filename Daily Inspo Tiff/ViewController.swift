f4d6dd96a93a91b83932c587d7671a4e9aa1a3c2
Girls Who Code <GWC@cs81-gwc-05.local>
2019-07-17 12:56:38 -0400

    Initial Commit

---
+++ Daily Inspo Tiff/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Daily Inspo Tiff
+//
+//  Created by Girls Who Code on 7/17/19.
+//  Copyright © 2019 Girls Who Code. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
